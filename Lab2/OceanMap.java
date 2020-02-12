d051b42ea266f230e99abe5521864392a3b6b58a
BalajiNaidu-Sadhu <54213777+BalajiNaidu-Sadhu@users.noreply.github.com>
2020-02-12 14:41:11 -0600

    Updated

--- Lab2/OceanMap.java
+++ Lab2/OceanMap.java
@@ -1,12 +1,12 @@
 
 import java.awt.Point;
-
+//class Oceanmap
 public class OceanMap {
 
     int xcoord ,ycoord  ;
-
+//instantiating the boolean array with 10*10
     boolean[][] myGrid = new boolean[10][10];
-
+// set method for the location
    public void setLocation(int i , int j){
         xcoord = i;
         ycoord = j;
@@ -15,6 +15,7 @@ public class OceanMap {
     public boolean[][] getMap() {
         return myGrid;
     }
+//     get method for the location
     public Point getLocation(){
         return new Point(xcoord,ycoord);
 
