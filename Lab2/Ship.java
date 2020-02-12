cc923a19db31ea5613ac088ed21d7d954c353e1b
BalajiNaidu-Sadhu <54213777+BalajiNaidu-Sadhu@users.noreply.github.com>
2020-02-12 14:43:36 -0600

    updated

--- Lab2/Ship.java
+++ Lab2/Ship.java
@@ -1,9 +1,10 @@
 import java.awt.Point;
-
+// class ship with the variables scale,xcoord,ycoord
 public class Ship {
     int scale = 50;
     int xcoord = 6;
     int ycoord = 6;
+//     Method is of point type towards east
     public Point goEast(int x , int y){
         if(xcoord >= 450){
             xcoord = xcoord;
@@ -15,6 +16,7 @@ public class Ship {
         }
         return new Point (xcoord , ycoord);
     }
+//     //     Method is of point type towards west
     public Point goWest(int x , int y){
         if(xcoord <= 0){
             xcoord = xcoord;
@@ -27,6 +29,7 @@ public class Ship {
         }
         return new Point(xcoord , ycoord);
     }
+//       Method is of point type towards north
     public Point goNorth(int x, int y) {
         if(ycoord <= 0){
             xcoord = xcoord;
@@ -38,6 +41,7 @@ public class Ship {
         }
         return new Point(xcoord , ycoord);
     }
+    //     Method is of point type towards south
     public Point goSouth(int x, int y) {
         if(ycoord >= 450){
             xcoord = xcoord;
